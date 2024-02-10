5cdb14c08dad8bc1bf8321e56304c95353cd893b
Menna-Elsallamy <mennaelsallamy@gmail.com>
2024-02-10 18:16:47 +0200

    test

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.module.css'
+export default function Footer() {
+  return (
+    <div className='bg-light p-3 text-center fixed-bottom'>Footer</div>
+
+    
+  )
+}
